51f1b7eefa4abe5711bb1d856e986cfbdf36d939
Cornbread <qwin44@hotmail.com>
2019-04-15 09:01:18 -0400

    Updated version downloaded

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const hbs = require("hbs");
+const fileUpload = require("express-fileupload");
+const debug = require("debug")("express:server");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 9001; // Its over 9000
+const dbLayer = require("./config/db");
 
-app.get('/', (req, res) => {
-    return res.send('Hello World!');
-});
-app.get('/home', (req, res) => {
-    return res.send('You are Home!');
-});
-app.get('/user/:username', (req, res) => {
-    return res.send(`You are ${req.param('username')}!`); // or param.username, current form deprecated
-});
+const rootRouter = require("./routes/root");
+const usersRouter = require("./routes/users");
+const numbersRouter = require("./routes/numbers");
 
-app.listen(port, () => console.log(`Sample App ${port}!`));
+const port = 9000;
+
+
+
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+
+
+app.use(bodyParser.json({ limit: "150mb" }));
+app.use(
+	bodyParser.urlencoded({
+		// to support URL-encoded bodies
+		limit: "150mb",
+		extended: true
+	})
+);
+
+app.use(
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: "/tmp/"
+	})
+);
+
+app.use(cookieParser());
+app.use("/inc", express.static(path.join(__dirname, "inc")));
+
+app.use((req, res, next) => {
+	if (req.cookies.vcount == undefined) {
+		req.vcount = 1;
+		res.cookie("vcount", 1);
+	} else {
+		req.vcount = parseInt(req.cookies.vcount) + 1;
+		res.cookie("vcount", req.vcount);
+	}
+	next();
+});
+app.use("/users", usersRouter);
+app.use("/", rootRouter);
+
+app.use("/numbers", numbersRouter);
+
+app.listen(port, function() {
+	dbLayer.init();
+	debug("Sample App " + port + "!");
+});
